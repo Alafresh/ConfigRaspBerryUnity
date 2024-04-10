71459511b6c0092a6a191340d07c753be2702306
Alafresh <alafresh16@gmail.com>
2024-04-09 19:50:41 -0500

    Update project

--- Assets/TalkToNPCs/Scripts/PlayerInteract.cs
+++ Assets/TalkToNPCs/Scripts/PlayerInteract.cs
@@ -2,35 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerInteract : MonoBehaviour {
+public class PlayerInteract : MonoBehaviour
+{
 
 
-    private void Update() {
-        if (Input.GetKeyDown(KeyCode.E)) {
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
             IInteractable interactable = GetInteractableObject();
-            if (interactable != null) {
+            if (interactable != null)
+            {
                 interactable.Interact(transform);
             }
         }
     }
 
-    public IInteractable GetInteractableObject() {
+    public IInteractable GetInteractableObject()
+    {
         List<IInteractable> interactableList = new List<IInteractable>();
         float interactRange = 3f;
         Collider[] colliderArray = Physics.OverlapSphere(transform.position, interactRange);
-        foreach (Collider collider in colliderArray) {
-            if (collider.TryGetComponent(out IInteractable interactable)) {
+        foreach (Collider collider in colliderArray)
+        {
+            if (collider.TryGetComponent(out IInteractable interactable))
+            {
                 interactableList.Add(interactable);
             }
         }
 
         IInteractable closestInteractable = null;
-        foreach (IInteractable interactable in interactableList) {
-            if (closestInteractable == null) {
+        foreach (IInteractable interactable in interactableList)
+        {
+            if (closestInteractable == null)
+            {
                 closestInteractable = interactable;
-            } else {
-                if (Vector3.Distance(transform.position, interactable.GetTransform().position) < 
-                    Vector3.Distance(transform.position, closestInteractable.GetTransform().position)) {
+            }
+            else
+            {
+                if (Vector3.Distance(transform.position, interactable.GetTransform().position) <
+                    Vector3.Distance(transform.position, closestInteractable.GetTransform().position))
+                {
                     // Closer
                     closestInteractable = interactable;
                 }
